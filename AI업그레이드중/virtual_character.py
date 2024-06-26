29051ec401ca43b196e0de0a4a646d8a070b09a6
KIMHYUNBIN <kimhunbin0226@gmail.com>
2024-06-26 14:14:33 +0900

    더 귀여운 디자인

--- AI업그레이드중/virtual_character.py
+++ AI업그레이드중/virtual_character.py
@@ -6,8 +6,8 @@ import speech_recognition as sr
 from gtts import gTTS
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from flask import Flask, request, jsonify, render_template, send_file
-import webbrowser  # 웹 브라우저 모듈 추가
-from threading import Timer  # 타이머 모듈 추가
+import webbrowser
+from threading import Timer
 
 # Initialize GPT model and tokenizer
 model_name = 'gpt2-large'  # gpt2-large 모델 선택
@@ -38,7 +38,7 @@ def play_wav_file(filename):
 
 # Function to convert text to speech using gTTS and save the result
 def make_tts(content):
-    tts = gTTS(text=content, lang='ko')
+    tts = gTTS(text=content, lang='en')
     tts.save('response.wav')
 
 # Function to generate response using GPT model
@@ -72,23 +72,21 @@ def chat():
     # Play the generated response (macOS)
     play_on_macos('response.wav')
 
-    # Return success message or any other response
-    return jsonify({'message': 'Response played.'}), 200
+    # Return AI response in the JSON
+    return jsonify({'response': response}), 200
 
 # macOS에서 WAV 파일 재생 함수
 def play_on_macos(filename):
     system_name = platform.system()
-    if system_name == 'Darwin':  # macOS인 경우
+    if (system_name == 'Darwin'):  # macOS인 경우
         os.system(f'afplay {filename}')
     else:
         print("Unsupported OS for audio playback.")
 
-# 함수로 웹 브라우저 자동 실행
 def open_browser():
     webbrowser.open_new('http://127.0.0.1:5000/')
 
 # Flask 애플리케이션 실행 부분
 if __name__ == '__main__':
-    # 타이머를 사용하여 웹 브라우저를 자동으로 엽니다.
     Timer(1, open_browser).start()
     app.run(port=5000)
